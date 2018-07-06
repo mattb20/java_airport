d5246a37d185a10d6f0f784d37af2f39e7e83133
mattb20 <matt_burgoyne@live.co.uk>
2018-07-06 11:58:37 +0100

    added importing of weather class into airport class

--- src/main/java/JAirport/Airport.java
+++ src/main/java/JAirport/Airport.java
@@ -3,7 +3,7 @@ package JAirport;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
+import JAirport.Weather;
 public class Airport {
     ArrayList<String> hanger = new ArrayList<String>();
     public ArrayList<String> storage() {
