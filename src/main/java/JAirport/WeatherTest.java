e28e8c70298ea23b5ee2e7cd0c3b8b918a85ddce
mattb20 <matt_burgoyne@live.co.uk>
2018-07-06 12:26:35 +0100

    rewrote weathertest to not use groovy, test failing due to lack of method in weather class

---
+++ src/main/java/JAirport/WeatherTest.java
@@ -0,0 +1,19 @@
+package JAirport;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+public class WeatherTest {
+    private Weather weather;
+
+    @BeforeEach
+    void setUp() {
+        weather = new Weather();
+    }
+
+    @Test
+    public void get_weather() {
+        weather.get();
+    }
+}
