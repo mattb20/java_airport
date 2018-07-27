332709ef33b3fe9d2290266ac7d4247b69b1958f
mattb20 <matt_burgoyne@live.co.uk>
2018-07-27 12:03:31 +0100

    modified pom.xml to include Junit dependency

--- src/main/java/JAirport/AirportTest.java
+++ src/main/java/JAirport/AirportTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
 public class AirportTest {
     private Airport airport;
     @BeforeEach
